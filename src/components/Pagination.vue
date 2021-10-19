88d0406f3b7fde888bd0d64a72b60eb2b9293095
ledinhdat710 <80235959+ledinhdat710@users.noreply.github.com>
2021-10-19 08:22:31 +0700

    Update Pagination.vue

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,70 +1,104 @@
 <template>
-  <nav>
-    <ul class="pagination">
-      <li v-if="pagination.current_page > 1">
-        <a
-          href="#"
-          aria-label="Previous"
-          v-on:click.prevent="changePage(pagination.current_page - 1)"
-        >
-          <span aria-hidden="true">«</span>
-        </a>
-      </li>
-      <li
-        v-for="page in pagesNumber"
-        :class="{ active: page == pagination.current_page }"
-        v-bind:key="page"
-      >
-        <a href="#" v-on:click.prevent="changePage(page)">{{ page }}</a>
-      </li>
-      <li v-if="pagination.current_page < pagination.last_page">
-        <a
-          href="#"
-          aria-label="Next"
-          v-on:click.prevent="changePage(pagination.current_page + 1)"
-        >
-          <span aria-hidden="true">»</span>
-        </a>
-      </li>
-    </ul>
-  </nav>
+  <div>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item">
+          <button type="button" class="page-link" @click="page = 1">
+            First Page
+          </button>
+        </li>
+        <li class="page-item">
+          <button
+            type="button"
+            class="page-link"
+            v-if="page != 1"
+            @click="page--"
+          >
+            Previous
+          </button>
+        </li>
+        <li class="page-item">
+          <button
+            type="button"
+            class="page-link"
+            v-for="pageNumber in pages.slice(
+              page > 1 ? page - 2 : page - 1,
+              page + 5
+            )"
+            v-bind:key="pageNumber.index"
+            @click="page = pageNumber"
+            :style="[
+              pageNumber == page ? { color: 'blue' } : { color: '#29b3ed' },
+            ]"
+          >
+            {{ pageNumber }}
+          </button>
+        </li>
+        <li class="page-item">
+          <button
+            type="button"
+            @click="page++"
+            v-if="page < pages.length"
+            class="page-link"
+          >
+            Next
+          </button>
+          <button type="button" class="page-link" @click="page = pages.length">
+            End Page
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
+
 <script>
 export default {
-  props: {
-    pagination: {
-      type: Object,
-      required: true,
+  data() {
+    return {
+      posts: [],
+      page: 1,
+      perPage: 10,
+      pages: [],
+    };
+  },
+  methods: {
+    setPages() {
+      let numberOfPages = Math.ceil(this.posts.length / this.perPage);
+      this.pages.length = 0;
+      for (let index = 1; index <= numberOfPages; index++) {
+        this.pages.push(index);
+      }
     },
-    offset: {
-      type: Number,
-      default: 4,
+    paginate(posts) {
+      let page = this.page;
+      let perPage = this.perPage;
+      let from = page * perPage - perPage;
+      let to = page * perPage;
+      return posts.slice(from, to);
+    },
+    sort() {
+      this.posts.sort((a, b) => b.id - a.id);
     },
   },
   computed: {
-    pagesNumber: function () {
-      if (!this.pagination.to) {
-        return [];
-      }
-      var from = this.pagination.current_page - this.offset;
-      if (from < 1) {
-        from = 1;
-      }
-      var to = from + this.offset * 2;
-      if (to >= this.pagination.last_page) {
-        to = this.pagination.last_page;
-      }
-      var pagesArray = [];
-      for (from = 1; from <= to; from++) {
-        pagesArray.push(from);
-      }
-      return pagesArray;
+    displayedPosts() {
+      return this.paginate(this.posts);
     },
   },
-  methods: {
-    changePage: function (page) {
-      this.pagination.current_page = page;
+  watch: {
+    posts() {
+      this.setPages();
     },
   },
 };
 </script>
+
+<style scoped>
+button.page-link {
+  font-size: 20px;
+  color: #1b1f20;
+  font-weight: 500;
+  display: inline-block;
+}
+</style>
